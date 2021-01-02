6ac3eaf5ad626b029e0d4eabe754fabf7e28bd17
Matthew Thomas <mzthomas96@gmail.com>
2021-01-02 17:53:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,9 +35,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def  update_element_from_index (array, index_number,element)
-  {
     array.delete_at(index_number)
     array.insert(index_number,element)
     return array[index_number]
-  }
 end
